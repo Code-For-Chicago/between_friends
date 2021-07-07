67c43c31c3673b826d9b5be5310553f2d4d7a1d3
rebecca <r.young1025@gmail.com>
2021-07-07 01:54:31 -0500

    center align board member info and fix column break

--- wp-content/themes/betweenfriendschicago/page.php
+++ wp-content/themes/betweenfriendschicago/page.php
@@ -169,7 +169,7 @@
 					)); ?>
 					<?php if( have_posts() ): while ( have_posts() ) : the_post(); ?>
 						<?php if( get_field('director_or_council') == 'director' ){ ?>
-						<div class="member-holder text-center text-md-left">
+						<div class="member-holder text-center">
 						<?php if(get_field('board_member_photo')){ ?>
 								<?php // Not sure if this is best practice, but it works
 									$image = get_field('board_member_photo');
@@ -205,7 +205,7 @@
 					)); ?>
 					<?php if( have_posts() ): while ( have_posts() ) : the_post(); ?>
 						<?php if( get_field('director_or_council') == 'council' ){ ?>
-							<div class="member-holder text-center text-md-left">
+							<div class="member-holder text-center">
 							<?php if(get_field('board_member_photo')){ ?>
 								<?php // Not sure if this is best practice, but it works
 									$image = get_field('board_member_photo');
